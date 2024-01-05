02facb7279b98965568b864a7ff6c3f284a6fd4b
slee <sanlee8495@gmail.com>
2024-01-05 13:49:20 -0800

    01052024

---
+++ code.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(lubridate)
+library(readxl)
+
+# Read in data
+weekly_hiring_ms_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2023/12.05.2023/Weekly Site Staffing Update 12-5-2023 .xlsx",
+                                           sheet = "Hiring MS DATA")
+
+weekly_hires_terms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2023/12.05.2023/Weekly Site Staffing Update 12-5-2023 .xlsx",
+                                        sheet = "Hires & Terms")
+
+# recap_ms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/Recap of Weekly Site Staffing Updates.xlsx",
+#                     sheet = "MS Recap")
+
+# recap_turnover <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/Recap of Weekly Site Staffing Updates.xlsx",
+#                              sheet = "Turnover Recap")
+
+
+
+readRDS("master_data_ms_rds.rds") -> master_data_ms_rds
+# readRDS("master_data_turnover_rds.rds") -> master_data_turnover_rds
+
+############################## Clean up data
+########## weekly_hiring_ms_data
+
+weekly_hiring_ms_data %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(report_date = lubridate::dmy(report_date)) %>% 
+  dplyr::filter(!is.na(external_openings)) %>% 
+  dplyr::rename(plant_name = plant) -> weekly_hiring_ms_data_cleaned
+
+# recap_ms %>%
+#   janitor::clean_names() %>%
+#   mutate(report_date = lubridate::date(report_date)) -> recap_ms_cleaned
+
+# rbind(recap_ms_cleaned, weekly_hiring_ms_data_cleaned) -> master_data_ms_rds
+rbind(master_data_ms_rds, weekly_hiring_ms_data_cleaned) -> master_data_ms_rds
+
+master_data_ms_rds %>%
+  mutate(row_id = apply(., 1, paste, collapse = "")) %>%
+  distinct(row_id, .keep_all = TRUE) %>%
+  select(-row_id) -> master_data_ms_rds
+
+saveRDS(master_data_ms_rds, "master_data_ms_rds.rds")
+
+
+writexl::write_xlsx(master_data_ms_rds, "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2023/12.05.2023/master_data_ms_rds.xlsx")
+
+
+########## weekly_hires_terms
+
+weekly_hires_terms %>% 
+  janitor::clean_names() %>% 
+  data.frame()
