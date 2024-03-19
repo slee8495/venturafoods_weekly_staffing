38d02358e094ed51692b13956a0bd5f5866d49bf
slee <sanlee8495@gmail.com>
2024-03-18 19:42:20 -0700

    03182024

--- code.R
+++ code.R
@@ -4,8 +4,8 @@ library(readxl)
 library(fs)
 
 # Define the paths
-old_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.04.2024"
-new_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.11.2024"
+old_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024"
+new_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024"
 file_name <- "Recap of Weekly Site Staffing Updates.xlsx"
 
 # Create the new directory
@@ -19,10 +19,10 @@ file.copy(file.path(old_dir, file_name), file.path(new_dir, file_name))
 ##############################################################################################################################################################
 
 # Read in data
-weekly_hiring_ms_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.11.2024/Weekly Site Staffing Update 03-05-24.xlsx",
+weekly_hiring_ms_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024/Weekly Site Staffing Update 03-12-24.xlsx",
                                            sheet = "Hiring MS DATA")
 
-weekly_hires_terms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.11.2024/Weekly Site Staffing Update 03-05-24.xlsx",
+weekly_hires_terms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024/Weekly Site Staffing Update 03-12-24.xlsx",
                                         sheet = "Hires & Terms")
 
 # recap_ms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/Recap of Weekly Site Staffing Updates.xlsx",
@@ -70,7 +70,7 @@ weekly_hiring_ms_data_cleaned %>%
                 "Internal Openings" = internal_openings,
                 "Pending BG/DS" = pending_bg_ds,
                 "Filled By Temps" = filled_by_temps) %>% 
-  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.11.2024/ms recap.xlsx")
+  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024/ms recap.xlsx")
 
 
 ########## weekly_hires_terms
@@ -103,7 +103,7 @@ weekly_hires_terms_cleaned %>% head(15) %>% rename(Hires = Value) -> hires
 
 
 cbind(terms, hires) %>% 
-  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.11.2024/Turnover Recap.xlsx")
+  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024/Turnover Recap.xlsx")
 
 # Making sure we are using the right month. 
 weekly_hires_terms %>% 
@@ -127,15 +127,19 @@ weekly_hires_terms %>%
 
 # In the Weekly Site File, go to "Birmingham" Tab, and check the date. 
 
+
+### Manual input number in MicroStrategy
+# Weekly Stie file -> FY24 Rolling tab -> Col CG21 (example: 36.52% - make sure once a month, it's applying the right range)
+# In MictoStrategy, go to Turnover YTD -> Turnover Rate -> below Plants Turnover
+# Also make sure  that all CG column is copied to "Recap.xlsx" file "Rolling 12 Month TO%" Tab correctly. 
+
+
 # Now go ahead to MicroStrategy and update the cube. 
 # https://edgeanalytics.venturafoods.com/MicroStrategyLibrary/app/DF007F1C11E9B3099BB30080EF7513D2/971A4F9F11EC4177EB3F0080EF1563A1/W81--K126/edit
 
 
 
 
-### Manual input number in MicroStrategy
-# Weekly Stie file -> FY24 Rolling tab -> Col CG21 (example: 36.52% - make sure once a month, it's applying the right range)
-# In MictoStrategy, go to Turnover YTD -> Turnover Rate -> below Plants Turnover
-# Also make sure  that all CG column is copied to "Recap.xlsx" file "Rolling 12 Month TO%" Tab correctly. 
+
 
 
