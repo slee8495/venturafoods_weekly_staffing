b1e98ccfc47f6abd235a392f74ea73aa526403ce
slee <sanlee8495@gmail.com>
2024-04-08 23:00:28 -0700

    04082024

--- code.R
+++ code.R
@@ -4,8 +4,8 @@ library(readxl)
 library(fs)
 
 # Define the paths
-old_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.18.2024"
-new_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024"
+old_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024"
+new_dir <- "C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/04.08.2024"
 file_name <- "Recap of Weekly Site Staffing Updates.xlsx"
 
 # Create the new directory
@@ -19,10 +19,10 @@ file.copy(file.path(old_dir, file_name), file.path(new_dir, file_name))
 ##############################################################################################################################################################
 
 # Read in data
-weekly_hiring_ms_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024/Weekly Site Staffing Update 03-19-24.xlsx",
+weekly_hiring_ms_data <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/04.08.2024/Weekly Site Staffing Update 04-02-24.xlsx",
                                            sheet = "Hiring MS DATA")
 
-weekly_hires_terms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024/Weekly Site Staffing Update 03-19-24.xlsx",
+weekly_hires_terms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/04.08.2024/Weekly Site Staffing Update 04-02-24.xlsx",
                                         sheet = "Hires & Terms")
 
 # recap_ms <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/Recap of Weekly Site Staffing Updates.xlsx",
@@ -70,7 +70,7 @@ weekly_hiring_ms_data_cleaned %>%
                 "Internal Openings" = internal_openings,
                 "Pending BG/DS" = pending_bg_ds,
                 "Filled By Temps" = filled_by_temps) %>% 
-  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024/ms recap.xlsx")
+  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/04.08.2024/ms recap.xlsx")
 
 
 ########## weekly_hires_terms
@@ -95,7 +95,7 @@ weekly_hires_terms %>%
                                          "Brantford"
                                         )) %>% 
   dplyr::mutate(across(c(1, ncol(.)), ~replace(., is.na(.), 0))) %>% 
-  dplyr::select(1, ncol(.)-3) %>% 
+  dplyr::select(1, ncol(.)-13) %>% 
   dplyr::rename(Location = names(.)[1], Value = names(.)[2]) -> weekly_hires_terms_cleaned_pre_week
 
 weekly_hires_terms_cleaned_pre_week %>% tail(15) %>% rename(Terms = Value) -> terms_preweek
@@ -122,18 +122,18 @@ weekly_hires_terms %>%
                                          "Brantford"
   )) %>% 
   dplyr::mutate(across(c(1, ncol(.)), ~replace(., is.na(.), 0))) %>% 
-  dplyr::select(1, ncol(.)-2) %>% 
+  dplyr::select(1, ncol(.)-13) %>% 
   dplyr::rename(Location = names(.)[1], Value = names(.)[2]) -> weekly_hires_terms_cleaned_pre_week
 
 weekly_hires_terms_cleaned_pre_week %>% tail(15) %>% rename(Terms = Value) -> terms_thisweek
 weekly_hires_terms_cleaned_pre_week %>% head(15) %>% rename(Hires = Value) -> hires_thisweek
 
 
-rbind(terms_preweek, terms_thisweek) %>% mutate_all(~replace_na(., 0)) -> terms
-rbind(hires_preweek, hires_thisweek) %>% mutate_all(~replace_na(., 0)) -> hires
+rbind(terms_preweek, terms_thisweek) %>% mutate(Terms = ifelse(is.na(Terms), 0, Terms)) -> terms
+rbind(hires_preweek, hires_thisweek) %>% mutate(Hires = ifelse(is.na(Hires), 0, Hires)) -> hires
 
 cbind(terms, hires) %>% 
-  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/03.25.2024/Turnover Recap.xlsx")
+  writexl::write_xlsx("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/Weekly Staffing/2024/04.08.2024/Turnover Recap.xlsx")
 
 # Making sure we are using the right month. 
 weekly_hires_terms %>% 
@@ -160,7 +160,7 @@ weekly_hires_terms %>%
 
 # In the Weekly Site File, go to "Birmingham" Tab, and check the date. 
 # Weekly file FY 24 rolling tab. make sure the formula is rolling 12 months. (CF Col)
-
+# Weekly CG col Copy, paste in Recap file "Rolling 12 Month TO%" Tab. 
 
 # https://edgeanalytics.venturafoods.com/MicroStrategyLibrary/app/DF007F1C11E9B3099BB30080EF7513D2/971A4F9F11EC4177EB3F0080EF1563A1/W81--K126/edit
 ### Manual input number in MicroStrategy
